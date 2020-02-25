f113a576959be793790d15b0511e69c8ffc7f183
Kyle Rubenok <kyle@rubenok.ca>
2020-02-24 19:12:38 -0500

    Add starting points

--- src/pages/points.js
+++ src/pages/points.js
@@ -45,6 +45,216 @@ const PointsPage = () => (
           daily here and in the Fac-O Participants group.
         </p>
       </div>
+      <table class="table table-bordered table-hover table-condensed">
+        <thead>
+          <tr>
+            <th title="Field #1">Rank</th>
+            <th title="Field #2">Team</th>
+            <th title="Field #3">Total</th>
+            <th title="Field #4">Scunt</th>
+            <th title="Field #5">Monday</th>
+            <th title="Field #6">Tuesday</th>
+            <th title="Field #7">Wednesday</th>
+            <th title="Field #8">Thursday</th>
+            <th title="Field #9">Friday</th>
+            <th title="Field #10">Bonus</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="right">1</td>
+            <td>PTOT</td>
+            <td>2,797</td>
+            <td>2,797</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">2</td>
+            <td>Science 2</td>
+            <td>2,735</td>
+            <td>2,485</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>250</td>
+          </tr>
+          <tr>
+            <td align="right">3</td>
+            <td>Arts 1</td>
+            <td>2,651</td>
+            <td>2,651</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">4</td>
+            <td>Education 1</td>
+            <td>2,596</td>
+            <td>2,596</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">5</td>
+            <td>Music</td>
+            <td>2,466</td>
+            <td>2,216</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>250</td>
+          </tr>
+          <tr>
+            <td align="right">6</td>
+            <td>Arts 3</td>
+            <td>2,166</td>
+            <td>1,916</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>250</td>
+          </tr>
+          <tr>
+            <td align="right">7</td>
+            <td>Science 1</td>
+            <td>2,158</td>
+            <td>1,908</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>250</td>
+          </tr>
+          <tr>
+            <td align="right">8</td>
+            <td>Engineering</td>
+            <td>1,951</td>
+            <td>1,701</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>250</td>
+          </tr>
+          <tr>
+            <td align="right">9</td>
+            <td>Education 2</td>
+            <td>1,682</td>
+            <td>1,682</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">10</td>
+            <td>Management 2</td>
+            <td>1,167</td>
+            <td>1,167</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">11</td>
+            <td>Law</td>
+            <td>1,033</td>
+            <td>1,033</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">12</td>
+            <td>Arts 4</td>
+            <td>949</td>
+            <td>949</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">13</td>
+            <td>Medicine</td>
+            <td>854</td>
+            <td>854</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">14</td>
+            <td>Arts 2</td>
+            <td>810</td>
+            <td>810</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">15</td>
+            <td>Management 1</td>
+            <td>598</td>
+            <td>598</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td align="right">16</td>
+            <td>Arts 5</td>
+            <td>198</td>
+            <td>198</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
     </StyledContainer>
     <Footer />
   </Layout>
