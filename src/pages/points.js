b847674dc30b8bd7c3ec5cfbca90c1f2ee53e5f2
Kyle Rubenok <kyle@rubenok.ca>
2020-03-12 11:56:08 -0400

    Add wednesday

--- src/pages/points.js
+++ src/pages/points.js
@@ -76,251 +76,194 @@ const PointsPage = () => {
               <tr>
                 <td align="right">1</td>
                 <td>Science 2</td>
-                <td>9,490</td>
+                <td>23,115</td>
                 <td>2,485</td>
                 <td>1,150</td>
                 <td>5,525</td>
+                <td>13,575</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>330</td>
+                <td>380</td>
               </tr>
               <tr>
                 <td align="right">2</td>
                 <td>Arts 1</td>
-                <td>9,171</td>
+                <td>18,516</td>
                 <td>2,651</td>
                 <td>1,275</td>
                 <td>4,925</td>
+                <td>9,150</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>320</td>
+                <td>515</td>
               </tr>
               <tr>
                 <td align="right">3</td>
-                <td>PTOT</td>
-                <td>8,147</td>
-                <td>2,797</td>
-                <td>650</td>
-                <td>4,525</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>175</td>
-              </tr>
-              <tr>
-                <td align="right">4</td>
-                <td>Music</td>
-                <td>8,141</td>
-                <td>2,216</td>
-                <td>1,225</td>
-                <td>4,300</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>400</td>
-              </tr>
-              <tr>
-                <td align="right">5</td>
                 <td>Arts 3</td>
-                <td>6,131</td>
+                <td>16,776</td>
                 <td>1,916</td>
                 <td>435</td>
                 <td>3,500</td>
+                <td>10,625</td>
                 <td>-</td>
                 <td>-</td>
+                <td>300</td>
+              </tr>
+              <tr>
+                <td align="right">4</td>
+                <td>PTOT</td>
+                <td>15,767</td>
+                <td>2,797</td>
+                <td>650</td>
+                <td>4,525</td>
+                <td>7,600</td>
                 <td>-</td>
-                <td>280</td>
+                <td>-</td>
+                <td>195</td>
+              </tr>
+              <tr>
+                <td align="right">5</td>
+                <td>Music</td>
+                <td>14,931</td>
+                <td>2,216</td>
+                <td>1,225</td>
+                <td>4,300</td>
+                <td>6,725</td>
+                <td>-</td>
+                <td>-</td>
+                <td>465</td>
               </tr>
               <tr>
                 <td align="right">6</td>
                 <td>Engineering</td>
-                <td>6,126</td>
+                <td>13,361</td>
                 <td>1,701</td>
                 <td>1,075</td>
                 <td>3,025</td>
+                <td>7,200</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>325</td>
+                <td>360</td>
               </tr>
               <tr>
                 <td align="right">7</td>
-                <td>Science 1</td>
-                <td>6,108</td>
-                <td>1,908</td>
-                <td>325</td>
-                <td>3,625</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>250</td>
-              </tr>
-              <tr>
-                <td align="right">8</td>
-                <td>Medicine</td>
-                <td>5,669</td>
-                <td>854</td>
-                <td>700</td>
-                <td>4,100</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>15</td>
-              </tr>
-              <tr>
-                <td align="right">9</td>
-                <td>Education 1</td>
-                <td>5,486</td>
-                <td>2,596</td>
-                <td>715</td>
-                <td>2,075</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>100</td>
-              </tr>
-              <tr>
-                <td align="right">10</td>
                 <td>Management 1</td>
-                <td>4,993</td>
+                <td>12,468</td>
                 <td>598</td>
                 <td>450</td>
                 <td>3,875</td>
-                <td>-</td>
+                <td>7,475</td>
                 <td>-</td>
                 <td>-</td>
                 <td>70</td>
               </tr>
               <tr>
-                <td align="right">11</td>
+                <td align="right">8</td>
+                <td>Management 2</td>
+                <td>10,707</td>
+                <td>1,167</td>
+                <td>375</td>
+                <td>2,350</td>
+                <td>6,775</td>
+                <td>-</td>
+                <td>-</td>
+                <td>40</td>
+              </tr>
+              <tr>
+                <td align="right">9</td>
                 <td>Education 2</td>
-                <td>4,962</td>
+                <td>10,187</td>
                 <td>1,682</td>
                 <td>915</td>
                 <td>2,300</td>
-                <td>-</td>
+                <td>5,225</td>
                 <td>-</td>
                 <td>-</td>
                 <td>65</td>
               </tr>
               <tr>
+                <td align="right">10</td>
+                <td>Science 1</td>
+                <td>9,933</td>
+                <td>1,908</td>
+                <td>325</td>
+                <td>3,625</td>
+                <td>3,825</td>
+                <td>-</td>
+                <td>-</td>
+                <td>250</td>
+              </tr>
+              <tr>
+                <td align="right">11</td>
+                <td>Education 1</td>
+                <td>9,376</td>
+                <td>2,596</td>
+                <td>715</td>
+                <td>2,075</td>
+                <td>3,825</td>
+                <td>-</td>
+                <td>-</td>
+                <td>165</td>
+              </tr>
+              <tr>
                 <td align="right">12</td>
                 <td>Arts 2</td>
-                <td>4,835</td>
+                <td>8,585</td>
                 <td>810</td>
                 <td>400</td>
                 <td>3,575</td>
+                <td>3,650</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>50</td>
+                <td>150</td>
               </tr>
               <tr>
                 <td align="right">13</td>
-                <td>Management 2</td>
-                <td>3,892</td>
-                <td>1,167</td>
-                <td>375</td>
-                <td>2,350</td>
+                <td>Medicine</td>
+                <td>8,269</td>
+                <td>854</td>
+                <td>700</td>
+                <td>4,100</td>
+                <td>2,600</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>-</td>
+                <td>15</td>
               </tr>
               <tr>
                 <td align="right">14</td>
-                <td>Arts 4</td>
-                <td>3,449</td>
-                <td>949</td>
-                <td>775</td>
-                <td>1,675</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td align="right">15</td>
                 <td>Arts 5</td>
-                <td>3,248</td>
+                <td>7,898</td>
                 <td>198</td>
                 <td>725</td>
                 <td>2,300</td>
+                <td>4,550</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-                <td>25</td>
+                <td>125</td>
               </tr>
               <tr>
-                <td align="right">16</td>
+                <td align="right">15</td>
                 <td>Law</td>
-                <td>3,073</td>
+                <td>7,498</td>
                 <td>1,033</td>
                 <td>665</td>
                 <td>1,375</td>
+                <td>4,425</td>
                 <td>-</td>
                 <td>-</td>
                 <td>-</td>
-                <td>-</td>
-              </tr>
-            </tbody>
-          </table>
-          <h2>Faculty</h2>
-          <table class="table table-bordered table-hover table-condensed">
-            <thead>
-              <tr>
-                <th title="Field #1">Rank</th>
-                <th title="Field #2">Faculty</th>
-                <th title="Field #3">Average</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td align="right">1</td>
-                <td>PTOT</td>
-                <td>9,490</td>
-              </tr>
-              <tr>
-                <td align="right">2</td>
-                <td>Music</td>
-                <td>9,171</td>
-              </tr>
-              <tr>
-                <td align="right">3</td>
-                <td>Science</td>
-                <td>6,817</td>
-              </tr>
-              <tr>
-                <td align="right">4</td>
-                <td>Engineering</td>
-                <td>6,131</td>
-              </tr>
-              <tr>
-                <td align="right">6</td>
-                <td>Medicine</td>
-                <td>6,108</td>
-              </tr>
-              <tr>
-                <td align="right">8</td>
-                <td>Arts</td>
-                <td>4,895</td>
-              </tr>
-              <tr>
-                <td align="right">9</td>
-                <td>Education</td>
-                <td>5,224</td>
-              </tr>
-              <tr>
-                <td align="right">10</td>
-                <td>Management</td>
-                <td>4,443</td>
               </tr>
               <tr>
                 <td align="right">16</td>
-                <td>Law</td>
-                <td>3,073</td>
+                <td>Arts 4</td>
+                <td>6,299</td>
+                <td>949</td>
+                <td>775</td>
+                <td>1,675</td>
+                <td>2,850</td>
+                <td>-</td>
+                <td>-</td>
+                <td>50</td>
               </tr>
             </tbody>
           </table>
